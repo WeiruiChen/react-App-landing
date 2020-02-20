93d91abe4ff45727f9372e2d292114aa4af25ee0
chenweirui <chenweirui@baidu.com>
2020-02-20 20:01:00 +0800

    定位调整

--- src/Home/Teams2.jsx
+++ src/Home/Teams2.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import QueueAnim from 'rc-queue-anim';
-import { Row, Col } from 'antd';
+import {Row, Col} from 'antd';
 import OverPack from 'rc-scroll-anim/lib/ScrollOverPack';
-import { getChildrenToRender } from './utils';
+import {getChildrenToRender} from './utils';
 
 class Teams1 extends React.PureComponent {
   getBlockChildren = (data) =>
     data.map((item, i) => {
-      const { titleWrapper, ...$item } = item;
+      const {titleWrapper, ...$item} = item;
       return (
         <Col key={i.toString()} {...$item}>
           {titleWrapper.children.map(getChildrenToRender)}
@@ -16,29 +16,32 @@ class Teams1 extends React.PureComponent {
     });
 
   render() {
-    const { ...props } = this.props;
-    const { dataSource } = props;
+    const {...props} = this.props;
+    const {dataSource} = props;
     delete props.dataSource;
     delete props.isMobile;
     const listChildren = this.getBlockChildren(dataSource.block.children);
     return (
-      <div {...props} {...dataSource.wrapper}>
+      <div>
         <a name="hezuohuoban"></a>
-        <div {...dataSource.page}>
-          <div {...dataSource.titleWrapper}>
-            {dataSource.titleWrapper.children.map(getChildrenToRender)}
+        <div {...props} {...dataSource.wrapper}>
+
+          <div {...dataSource.page}>
+            <div {...dataSource.titleWrapper}>
+              {dataSource.titleWrapper.children.map(getChildrenToRender)}
+            </div>
+            <OverPack {...dataSource.OverPack}>
+              <QueueAnim
+                type="bottom"
+                key="block"
+                leaveReverse
+                {...dataSource.block}
+                component={Row}
+              >
+                {listChildren}
+              </QueueAnim>
+            </OverPack>
           </div>
-          <OverPack {...dataSource.OverPack}>
-            <QueueAnim
-              type="bottom"
-              key="block"
-              leaveReverse
-              {...dataSource.block}
-              component={Row}
-            >
-              {listChildren}
-            </QueueAnim>
-          </OverPack>
         </div>
       </div>
     );
